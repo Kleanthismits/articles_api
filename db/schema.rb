13d9946e7eab2011a57bbb549afa3281e326159d
kmitsioulis <kleanthis.mitsioulis@persado.com>
2022-02-12 16:08:22 +0200

    Authentication: Add password to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_09_155734) do
+ActiveRecord::Schema.define(version: 2022_02_12_112346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2022_01_09_155734) do
     t.string "provider"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password"
     t.index ["login"], name: "index_users_on_login", unique: true
   end
 
